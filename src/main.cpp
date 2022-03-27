2920ef7f722ec460f5333031e2af934e08614f28
Dorian Ayoul <dorian.ayoul@epitech.eu>
2022-03-27 19:39:08 +0200

    [ADD] -h

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,18 @@
 
 int main(int ac, char **av)
 {
-    if (ac != 3 || !std::atoi(av[1])) {
-        std::cerr << "[Error] Provide a valid size of simulation and a scale" << std::endl;
-        return 84;
+    if ((ac == 2 && std::string(av[1]) == "-h") || ac != 3 ||
+        !std::atoi(av[1]) || !std::atoi(av[2])) {
+        std::cout << "\033[1;34mBluid\033[0m\n\tA fluid simulation program made"
+        << " in C++ with SFML.\n\tCreated for an Epitech game jam in 2 days by "
+        << "4 2nd year students:\n\tPierre HAMEL, Dorian AYOUL, Pierre MAUGER &"
+        << " Xavier TONNELIER\n\nARGUMENTS\n\t-h\tsee help\n\t[size] [scale]\t"
+        << "start with a valid simulation size and scale (resolution)\n\n"
+        << "USER INTERACTION\n\tDrag left click across the top-left simulation "
+        << "square\n\tto manipulate the fluid. Drag left click on a top-right\n"
+        << "\tsimulation parameter slider to change its value.\n\tPress ESCAPE "
+        << "to end the simulation." << std::endl;
+        return 0;
     }
     BluidEngine engine(std::atoi(av[1]), std::atoi(av[2]));
 
